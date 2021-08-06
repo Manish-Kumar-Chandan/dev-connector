f10f7a278ad3139e7969a6d18a2a08025fc542e4
Manish <Manish@LAPTOP-D8GQ0P6B>
2021-08-07 03:08:36 +0530

    login user done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Auth = require('../../middleware/auth');
 const User = require('../../model/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult} = require('express-validator');
 
 router.get('/', Auth.auth,async(req, res)=>{
     try {
@@ -10,6 +14,49 @@ router.get('/', Auth.auth,async(req, res)=>{
     } catch (error) {
         console.log(error)
         res.status(500).json({msg:'Server Error'});
+    }
+})
+
+//Login
+router.post('/',
+[
+    check('email', 'Please include a valid email')
+    .isEmail(),
+    check('password', 'Password is Required').exists()
+],
+ async(req, res)=>{
+    const {email, password}=req.body; 
+    const errors = validationResult(req); 
+    if(!errors.isEmpty()){
+        return res.status(400).json({error:errors.array()});
+    }
+    try {
+        //finding user
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:[{msg:"Invalid Credentials"}] });
+        }
+        
+        //matching password
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({error:[{msg:"Invalid Credentials"}] });
+        }
+
+        //return jsonwebtoken
+        const payload = {
+            user:{
+                id: user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'),{expiresIn:3600},(err, token)=>{
+            if(err) throw err
+            res.json({token})
+        })
+        
+        //return res.status(200).json({msg:"User Register Successfully"});
+    } catch (error) {
+        return res.status(324).json({error:error.message})
     }
 })
 
